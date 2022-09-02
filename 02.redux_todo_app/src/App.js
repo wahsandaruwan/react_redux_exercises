acccfcb0898c6830c56cfcf22bab7a56e74a27f4
wahsandaruwan <wahsandaruwan6@gmail.com>
2022-09-03 03:36:15 +0530

    Todo app html part done

---
+++ 02.redux_todo_app/src/App.js
@@ -0,0 +1,16 @@
+import Todo from './Components/Todo'
+
+const App = () => {
+  return (
+    <>
+      <h1>Todo App</h1>
+      <input type="text" />
+      <button>Add</button>
+      <br />
+      <br />
+      <Todo />
+    </>
+  )
+}
+
+export default App
---
+++ 02.redux_todo_app/src/Components/Todo.js
@@ -0,0 +1,14 @@
+const Todo = () => {
+  return (
+    <>
+      <div>
+        <p>Todo Name</p>
+        <p>Date & Time</p>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
+    </>
+  )
+}
+
+export default Todo
